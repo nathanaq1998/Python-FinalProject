d40a131d026c63dfff9b9854252f5057c2441ab8
Nathan <nathanaq1998@gmail.com>
2020-10-15 14:53:40 -0400

    Intial commit

--- keys.py
+++ keys.py
@@ -1,6 +1,6 @@
-consumer_key = 'jZFlWtFBnBVAVKh949hlvlfLh'
-consumer_key_secret = 'RU28fsT5UTfTYdwAjJhFD8IvSBXIrUtmIv3y0PHmKiP8fTvKe1'
-access_token = '1186255606241476613-u49JcPQ5COW5VEETr2pW9rE1dPeSXm'
-access_token_secret = 'Svp7ksraQoO3NLneHms08pWWyG2OfVkGmgsPj4dZDh8Pg'
+consumer_key = 'Put your twitter dev key here'
+consumer_key_secret = 'Put your twitter dev key here'
+access_token = 'Put your twitter dev token here'
+access_token_secret = 'Put your twitter dev token here'
 
 mapquest_key = 'YourAPIKey'
